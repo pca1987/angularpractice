b22b0a641fcd8df888bea02cafbf7d530722b260
pca1987 <pca1987@gmail.com>
2018-07-21 12:06:42 -0400

    Testing momentjs conversion to local timezone

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as moment from 'moment';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  constructor() {
+export class AppComponent implements OnInit {
+  date: string;
 
+  constructor() {
   } 
 
+  ngOnInit(): void {
+    let myDate: string = "2018-07-21 16:01:09 +0";
+
+    this.date = moment(myDate).toLocaleString();
+  }
 }
